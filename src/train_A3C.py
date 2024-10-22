7ab993e026ed07ccb9ff01eff73019246a231376
Kudrevatyh Pavel <kudrevatyhpol39@gmail.com>
2024-10-22 22:48:41 +0300

    Added visual for last

--- src/train_A3C.py
+++ src/train_A3C.py
@@ -1,6 +1,8 @@
 import torch
 import multiprocessing as mp
-import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
 
 from src.A3C import A3C
 from src.utils import SharedAdam, evaluate_policy
@@ -11,13 +13,14 @@ from world.realm import Realm
 from world.map_loaders.single_team import SingleTeamLabyrinthMapLoader
 
 import os
+
 os.environ["OMP_NUM_THREADS"] = "1"
 
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     gnet = A3C().to(device)  # global network
     gnet.share_memory()  # share the global parameters in multiprocessing
-    opt = SharedAdam(gnet.parameters(), lr=1e-3)  # global optimizer
+    opt = SharedAdam(gnet.parameters(), lr=1e-4)  # global optimizer
     global_episode_counter, global_episode_rewards, results_queue = mp.Value("i", 0), mp.Value("d", 0.0), mp.Queue()
     # parallel training
     workers = [
@@ -33,9 +36,6 @@ if __name__ == "__main__":
             break
     [w.join() for w in workers]
 
-    plt.plot(res)
-    plt.ylabel("Moving average ep reward")
-    plt.xlabel("Step")
-    plt.show()
-
+    plot = sns.lineplot(x=np.arange(len(res)), y=res)
+    plot.get_figure().savefig('training.jpg')
     evaluate_policy(agent=gnet, env=OnePlayerEnv(Realm(SingleTeamLabyrinthMapLoader(), 1)), device=device, episodes=1)
